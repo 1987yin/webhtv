1bc51bc757b4a14897b9a30e168b7c3db1a7384b
1987yin <yin0091@qq.com>
2026-09-14 07:30:34 +0800

    Update VideoActivity.java

--- app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
@@ -7484,10 +7484,9 @@ private final Task.Scope mPersonalRecommendationTasks = new Task.Scope(Task.reco
                 requestIntroSkipPlan();
                 if (!pendingResumeSeekApplied) applyAutoIntroSkip();
                 setAdFeedbackVisible(); // 播放地址确定后按格式刷新"有广告"按钮
-<<<<<<< HEAD
-=======
+
                 if (!mShortDramaQueueSessionActive) beginShortDramaQueue();
->>>>>>> upstream/main
+
                 break;
             case Player.STATE_ENDED:
                 if (mShortDramaQueue.shouldSuppressLegacyAutoAdvance()) {
